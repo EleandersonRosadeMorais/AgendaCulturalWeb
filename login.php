dcc58499c8cfe2954ea971fd27764d26c827a88e
Bruno <brunomodel60@gmail.com>
2025-11-26 21:38:48 -0300

    Implementando telas de gerenciador para admin

--- login.php
+++ login.php
@@ -14,7 +14,7 @@ $username = '';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login - Agenda Cultural</title>
-    <link rel="stylesheet" href="css/index.css">
+    <link rel="stylesheet" href="css/login.css">
 </head>
 <body>
     <div class="login-container">
